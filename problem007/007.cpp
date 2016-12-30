431107e993a897e8e9f2e7f08745e9347f0223d5
Joseph Schell <alejandro1one@hotmail.com>
2016-12-29 22:05:34 -0800

    Create new folder

---
+++ problem007/007.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int count = 0; 
+    int num = 2;
+    int limit = 10001;
+    
+    while (count < limit) {
+        bool prime = true;
+        
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                prime = false;
+                break;
+            }
+        }
+        if (prime == true) {
+            count++;
+        }
+        if (count!=limit){
+            num++;
+        }
+    }
+    
+    std::cout << "the prime number is " << num << ".";
+    return 0;
+}
